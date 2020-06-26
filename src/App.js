734d38e7770c27ece242e9e70468eb868b971d25
Mohammad Amiri <mohmd.a.a@gmail.com>
2020-06-26 09:11:14 -0700

    Configuring React

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import './App.css';
+
+class App extends Component {
+  render() {
+      return (
+          <div className="App">
+              <Navbar dark color="secondary">
+              <div className="container">
+                  <NavbarBrand href="/">Mohammad Amiri</NavbarBrand>
+              </div>
+              </Navbar>
+          </div>
+      );
+  }
+}
+
+
+
+export default App;
